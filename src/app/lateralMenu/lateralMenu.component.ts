e6328b4bd4b682abfec3e0a1a98db29c215a3e5b
cristian-avendano <cristian-avendano@upc.edu.co>
2021-08-06 22:50:47 -0500

    first commit

---
+++ src/app/lateralMenu/lateralMenu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-lateral-menu',
+    templateUrl: './lateralMenu.component.html',
+    styleUrls: ['./lateralMenu.component.sass']
+  })
+
+export class LateralMenuComponent implements OnInit{
+    
+    constructor(){    
+    }
+    
+    ngOnInit(){
+    }
+}
