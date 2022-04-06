5a063a1e2e041d2ce325196655185e60e7949b77
cristian.fernando.avendano.ramirez <cristian-avendano@hotmail.com>
2022-04-05 23:14:50 -0500

    adicion pantalla principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,18 @@ import { LateralMenuComponent } from './lateralMenu/lateralMenu.component';
 import { IconoComponent } from './lateralMenu/icono/icono.component';
 import { SectionComponent } from './section/section.component';
 import { PersonalInfoSectionComponent } from './section/sections/personal-info-section/personalInfoSection.component';
+import { StudiesComponent } from './section/sections/studies/studies';
+import { HomeComponent } from './section/sections/home/home';
 
 @NgModule({
   declarations: [
+    HomeComponent,
     AppComponent,
     LateralMenuComponent,
     IconoComponent,
     SectionComponent,
-    PersonalInfoSectionComponent
+    PersonalInfoSectionComponent,
+    StudiesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/section/sections/home/home.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.html',
+    styleUrls: ['./home.sass']
+  })
+
+export class HomeComponent implements OnInit{
+    
+    constructor(){    
+    }
+    
+    ngOnInit(){
+    }
+}
